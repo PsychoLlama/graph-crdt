1346614537c094a74ff720cb122004e2be6992e7
Jesse Gibson <Jesse_Gibson@me.com>
2016-07-24 20:20:46 -0600

    Add `raw` and static `from` methods, finish `merge`
    New `raw` method returns either the node at that key or null.
    Purposefully not called `read`, since another method will take that
    place, and automatically upconvert key nodes into pseudomerged ones.
    New static method `from` takes an object filled with nodes and plugs
    them into a new graph.
    New `merge` method takes a graph (or converts an object) and merges it
    into itself. Any nodes that already exist in the graph will merge also.
    Under the hood, merge just calls `add`.
    The `add` method now converts POJOs into Node instances with
    `Node.from`.
    All tests passing.

--- src/Graph/index.js
+++ src/Graph/index.js
@@ -4,6 +4,7 @@
  * @module graph-datastore.Graph
  */
 const Emitter = require('eventemitter3');
+const Node = require('../Node');
 const Symbol = require('es6-symbol');
 
 const nodes = Symbol('graph node container');
@@ -24,6 +25,27 @@ class Graph extends Emitter {
 		return new Graph();
 	}
 
+	/**
+	 * Turn an object with nodes in it into a graph.
+	 *
+	 * @param  {Object} object - An object with nothing
+	 * but nodes inside.
+	 * @returns {Graph} - A new graph containing all the nodes.
+	 */
+	static 'from' (object) {
+
+		const graph = Graph.create();
+
+		for (const key in object) {
+			if (object.hasOwnProperty(key)) {
+				const node = object[key];
+				graph.add(node);
+			}
+		}
+
+		return graph;
+	}
+
 	constructor () {
 		super();
 
@@ -31,17 +53,33 @@ class Graph extends Emitter {
 	}
 
 	/**
-	 * Add a node to the graph.
+	 * Return the unmodified value of a node lookup.
+	 *
+	 * @param  {String} key - The name/uid of the node.
+	 * @returns {Node|null} - The node if found, otherwise `null`.
+	 */
+	raw (key) {
+		return this[nodes][key] || null;
+	}
+
+	/**
+	 * Add a node to the graph, merging if it already exists.
 	 *
 	 * @emits Graph#event:add
 	 * @param  {Node} node - The data to add.
 	 * @returns {Graph} - The context object.
 	 */
 	add (node) {
+
+		if (!(node instanceof Node)) {
+			node = Node.from(node);
+		}
+
 		const { uid } = node.meta();
 
-		/** Make sure the node isn't already inside. */
-		if (this[nodes][uid]) {
+		const existing = this[nodes][uid];
+		if (existing) {
+			existing.merge(node);
 			return this;
 		}
 
@@ -50,6 +88,32 @@ class Graph extends Emitter {
 		this.emit('add', node);
 
 		return this;
+	}
+
+	/**
+	 * Merge one graph with another (graph union operation).
+	 * Calls add under the hood.
+	 *
+	 * @param  {Object} graph - The graph to merge with.
+	 * Items must be enumerable, and cannot be inherited from prototypes.
+	 * @returns {Graph} - The `this` context.
+	 */
+	merge (graph) {
+
+		/** Ensure it's a graph. */
+		if (!(graph instanceof Graph)) {
+			graph = Graph.from(graph);
+		}
+
+		const keys = graph.keys();
+
+		keys.forEach((key) => {
+			const node = graph.raw(key);
+			this.add(node);
+		});
+
+		return this;
+
 	}
 
 	/**
--- src/Graph/spec.js
+++ src/Graph/spec.js
@@ -5,11 +5,44 @@ const Graph = require('./index');
 const Node = require('../Node');
 const expect = require('expect');
 
+describe('Graph static method', () => {
+
+	describe('"from"', () => {
+
+		it('should convert an object into a graph', () => {
+			const node = Node.create();
+			const result = Graph.from({
+				[node]: node,
+			});
+
+			expect(result).toBeA(Graph);
+			expect(result.keys().length).toBe(1);
+		});
+
+		it('should upgrade objects within into nodes', () => {
+			const result = Graph.from({
+				'uid': {},
+			});
+
+			const [key] = result.keys();
+			expect(result.raw(key)).toBeA(Node);
+		});
+
+	});
+
+});
+
 describe('A graph', () => {
 	let graph;
 
 	beforeEach(() => {
 		graph = Graph.create();
+	});
+
+	it('should be initialized empty', () => {
+		const keys = graph.keys();
+
+		expect(keys.length).toBe(0);
 	});
 
 	describe('"add" call', () => {
@@ -54,8 +87,78 @@ describe('A graph', () => {
 			expect(emitted).toBe(false);
 		});
 
+		it('should merge nodes with the same uid', () => {
+			graph.add(node);
+			const similar = Node.create();
+
+			// FUTURE: when you can choose the uid, make this less hacky.
+			similar.meta().uid = node.meta().uid;
+
+			// Add a new property.
+			similar.merge({ hello: 'world' });
+
+			// Should merge with `node`.
+			graph.add(similar);
+
+			expect(node.read('hello')).toBe('world');
+		});
+
+		it('should convert objects to nodes', () => {
+			graph.add({ hello: 'graph' });
+
+			const [key] = graph.keys();
+			expect(graph.raw(key)).toBeA(Node);
+		});
+
 		it('should return the `this` context', () => {
 			const result = graph.add(node);
+
+			expect(result).toBe(graph);
+		});
+
+	});
+
+	describe('"raw" call', () => {
+
+		let node;
+
+		beforeEach(() => {
+			node = Node.create();
+			graph.add(node);
+		});
+
+		it('should return existing nodes', () => {
+			const result = graph.raw(node.toString());
+
+			expect(result).toBe(node);
+		});
+
+	});
+
+	describe('merge', () => {
+
+		let node1, node2, subgraph;
+
+		beforeEach(() => {
+			node1 = Node.create();
+			node2 = Node.create();
+
+			subgraph = Graph.from({
+				[node1]: node1,
+				[node2]: node2,
+			});
+		});
+
+		it('should add all the items in the subgraph', () => {
+			graph.merge(subgraph);
+
+			const keys = graph.keys();
+			expect(keys).toContain(node1.toString());
+			expect(keys).toContain(node2.toString());
+		});
+
+		it('should return the `this` context', () => {
+			const result = graph.merge(subgraph);
 
 			expect(result).toBe(graph);
 		});
