f166539049d87d06824d8b62f17b923089d351b1
Jesse Gibson <Jesse_Gibson@me.com>
2016-09-24 14:34:49 -0600

    Simplify conflict resolution.
    Instead of doing all sorts of weird comparisons, I figured out
    everything works (mostly) the same with much less code, and more
    predictably, if I only compare the stringified values. Since "{" is
    greater than the alphanumeric character set, graph edges are
    automatically preferred over most primitive values. However, if you save
    something like emojis or chinese characters as the first symbol in a
    string, it'll be favored over the edge.

--- src/union/index.js
+++ src/union/index.js
@@ -1,109 +1,40 @@
 /**
- * Deterministic merge algorithm.
+ * Utilities for deterministically merging node attributes.
  *
  * @private
  * @module graph-datastore/src/union
  */
 
-// NOTE: Any changes to the behavior of this file may have
-// disastrous consequences. This file ensures eventual consistency
-// across machines.
-
 /**
- * Determine whether a value is a type of object.
+ * Deterministically resolves merge conflicts
+ * on JSON-compatible data.
  *
- * @param  {Mixed} value - The value to test.
- * @returns {Boolean} - Whether it's a type of object.
- */
-function isObject (value) {
-
-	return value instanceof Object;
-}
-
-/**
- * Deterministically resolves merge conflicts.
- *
- * NOTE: Only subsets of JSON are supported.
- *
- * @throws {Error} - If an unplanned edge case is reached, debugging
- * information is thrown. Realistically, it should never happen.
- * @param  {field1} field1 - Field metadata for the current state.
- * @param  {field2} field2 - Field metadata for the update.
- * @returns {field1|field2} - The field that wins the conflict.
+ * @param  {Object} field1 - Current state field metadata.
+ * @param  {Object} field2 - Update field metadata.
+ * @returns {Object} - The greater value, or if they're
+ * equal, the current state.
  */
 export function conflict (field1, field2) {
-	const { value: current } = field1;
-	const { value: update } = field2;
 
-	// It doesn't matter if they're both the same.
-	if (current === update) {
-		return field1;
-	}
-
-	// Check to see if they're both object types.
-	if (isObject(current) && isObject(update)) {
-
-		// NOTE: uid coercion requires object types
-		// to use the `toString` interface.
-		const uid = {
-			current: String(current),
-			update: String(update),
-		};
-
-		// Compare objects by their uids.
-		if (uid.current > uid.update) {
-			return field1;
-		}
-		if (uid.update > uid.current) {
-			return field2;
-		}
-
-		// Both uids are the same, so it shouldn't
-		// matter which we choose.
-		return field1;
-	}
-
-	// If one is an object, favor it over the primitive.
-	if (isObject(current)) {
-		return field1;
-	}
-	if (isObject(update)) {
-		return field2;
-	}
-
-	// Neither is an object. Compare the string value.
-	const json = {
-		current: JSON.stringify(current),
-		update: JSON.stringify(update),
+	/** Turn the values into comparable strings. */
+	const string = {
+		current: JSON.stringify(field1.value),
+		update: JSON.stringify(field2.value),
 	};
 
-	// Compare values lexicographically.
-	if (json.current > json.update) {
-		return field1;
-	}
-	if (json.update > json.current) {
-		return field2;
-	}
+	/** Are the values equal? */
+	const equal = (string.current === string.update);
 
-	throw new Error(
-`Cannot resolve merge conflict on invalid JSON.
+	/** Is our current value greater? */
+	const greater = (string.current > string.update);
 
-If the data is valid JSON, please open an issue at
-https://github.com/PsychoLlama/merge-helper/issues
-and include the following debugging information:
-typeof current: "${typeof current}"
-typeof update: "${typeof update}"
-
-> If the following is sensitive, you can omit it.
-json.current: "${json.current}"
-json.update: "${json.update}"`
-	);
+	/** Return the winning value. */
+	return (equal || greater) ? field1 : field2;
 }
 
 /**
  * Render object state given a current time and mutation timeline.
  *
- * @todo rewrite with TDD.
  * @param  {Object} timeline - An object representing historical
  * transformations on a field.
  * @param  {Number} clock - The current computer timestamp.
--- src/union/spec.js
+++ src/union/spec.js
@@ -17,29 +17,30 @@ describe('A union', () => {
 		});
 
 		function setup () {
+
 			// Try it in both orders.
 			result = conflict(current, update);
 			inverse = conflict(update, current);
 		}
 
 		it('should resolve to the greater object uid', () => {
-			current.value = { toString: () => 'abc' };
-			update.value = { toString: () => 'def' };
+			current.value = { edge: 'abc' };
+			update.value = { edge: 'def' };
 			setup();
 			expect(result).toBe(update);
 			expect(inverse).toBe(update);
 		});
 
 		it('should return the first arg if uids are equal', () => {
-			current.value = { toString: () => 'equal' };
-			update.value = { toString: () => 'equal' };
+			current.value = { edge: 'equal' };
+			update.value = { edge: 'equal' };
 			setup();
 			expect(result).toBe(current);
 			expect(inverse).toBe(update);
 		});
 
 		it('should favor objects over strings', () => {
-			current.value = { toString: () => 'def' };
+			current.value = { edge: 'def' };
 			update.value = 'abc';
 			setup();
 			expect(result).toBe(current);
@@ -72,26 +73,20 @@ describe('A union', () => {
 
 		it('should toString objects to compare', () => {
 			const spy = createSpy().andReturn('abc');
-			current.value = { toString: spy };
-			update.value = { toString: () => 'def' };
+			current.value = { toJSON: spy };
+			update.value = { toJSON: () => 'def' };
 			setup();
 			expect(result).toBe(update);
 			expect(inverse).toBe(update);
 			expect(spy).toHaveBeenCalled();
 		});
 
-		it('should favor objects over strings of the same signature', () => {
-			current.value = { toString: () => 'equal' };
+		it('should favor edges over strings of the same value', () => {
+			current.value = { edge: 'equal' };
 			update.value = 'equal';
 			setup();
 			expect(result).toBe(current);
 			expect(inverse).toBe(current);
-		});
-
-		it('should throw an error for invalid json', () => {
-			current.value = Infinity;
-			update.value = NaN;
-			expect(setup).toThrow(/invalid json/i);
 		});
 
 	});
